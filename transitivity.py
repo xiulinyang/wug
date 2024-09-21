6ca0273da660d1f62db3dd8447ca8d51097316bd
Xiulin Yang <xiulinyang@Xiulins-MacBook-Pro-3.local>
2024-09-21 15:09:33 -0400

    update data

--- transitivity.py
+++ transitivity.py
@@ -3,8 +3,9 @@ import spacy
 from transformers import pipeline, AutoTokenizer, AutoModelForMaskedLM
 
 nlp = spacy.load('en_core_web_sm')
-model = 'google-bert/bert-base-uncased'
+# model = 'google-bert/bert-base-uncased'
 
+model = 'openai-community/gpt2'
 tokenizer = AutoTokenizer.from_pretrained(model)
 
 
@@ -12,29 +13,35 @@ transitivity = pd.read_csv('verb_transitivity.tsv', sep='\t')[['verb', 'intrans'
 transitivity_sorted = transitivity.sort_values(by='intrans', ascending=False)
 transitivity_sorted = transitivity_sorted.sort_values(by='percent_intrans', ascending=False)[:10000][['verb']].values.tolist()
 transitivity_sorted = [y for x in transitivity_sorted for y in x]
-with open('intransitive_tokens.txt', 'r') as t, open('multi_tokens.txt', 'w') as sa:
-    for token in t.readlines():
-        lemma_token = token.strip()
-        third_person = token.strip()+'s'
-        tokenized_lemma = tokenizer.tokenize(token)
-        tokenized_third_person = tokenizer.tokenize(third_person)
-        # print(tokenized_third_person, tokenized_lemma)
-        # if len(tokenized_third_person) == len(tokenized_lemma):
-        #     pass
-        #     # sa.write(f'{lemma_token}\n')
-        # else:
-        #     print(tokenized_third_person, tokenized_lemma)
-        #     print(lemma_token)
-        #     sa.write(f'{lemma_token}\n')
-        if len(tokenized_lemma)>1:
-            print(tokenized_third_person, tokenized_lemma)
-            print(lemma_token)
-            sa.write(f'{lemma_token}\n')
+# with open('intransitive_tokens.txt', 'r') as t, open('multi_tokens.txt', 'w') as sa:
+#     for token in t.readlines():
+#         lemma_token = token.strip()
+#         third_person = token.strip()+'s'
+#         tokenized_lemma = tokenizer.tokenize(token)
+#         tokenized_third_person = tokenizer.tokenize(third_person)
+#         # print(tokenized_third_person, tokenized_lemma)
+#         # if len(tokenized_third_person) == len(tokenized_lemma):
+#         #     pass
+#         #     # sa.write(f'{lemma_token}\n')
+#         # else:
+#         #     print(tokenized_third_person, tokenized_lemma)
+#         #     print(lemma_token)
+#         #     sa.write(f'{lemma_token}\n')
+#         if len(tokenized_lemma)>1:
+#             print(tokenized_third_person, tokenized_lemma)
+#             print(lemma_token)
+#             sa.write(f'{lemma_token}\n')
 
 # for x in tokenizer.vocab:
 #     if x[-1] =='s' and x in transitivity_sorted:
 #         print(x)
-
+with open('diff_tokens.txt', 'r') as d:
+    for t in d.readlines():
+        tokenized = tokenizer(t.strip())
+        tokenized2 = tokenizer(t.strip()+'s')
+        if len(tokenizer(t.strip())) == len(tokenizer(t.strip()+'s')):
+            print(t)
+            print(tokenizer.convert_ids_to_tokens(tokenized.input_ids), tokenizer.convert_ids_to_tokens(tokenized2.input_ids))
 
 '''
 lemmatize possible intransitive verbs
